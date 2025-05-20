ecd61a5cf65e5dd37d04f187da069996d10fced6
bindu0017 <beennduuthapa17@gmail.com>
2025-05-20 12:39:35 +0545

    initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Dog myDog = new Dog();
+        myDog.eat();  // From Animal class
+        myDog.bark(); // From Dog class
+    }
+}
+
+    
---
+++ src/animal.java
@@ -0,0 +1,12 @@
+class animal {
+    void eat() {
+        System.out.println("The animal eats food.");
+    }
+}
+
+class Dog extends animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+
+}
